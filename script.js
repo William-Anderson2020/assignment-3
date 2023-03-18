b91b99a6efab13e2ce732fdbe5901c98dbe3ccf4
William Anderson <48596907+William-Anderson2020@users.noreply.github.com>
2023-03-18 15:57:49 -0400

    Add comments to all functions
    Document all functions
    Fixed minor bug in addC where adding column while table is empty would add a two cell row rather than one

--- script.js
+++ script.js
@@ -8,17 +8,23 @@ let cellString = `<td onclick="alert('Clicked a table cell');"></td>`
 // Add a row
 function addR() {
     let cells = "";
+    //Check if table is empty
     if(tableTop.childElementCount > 0){
         let table = tableTop.firstElementChild;
+        //Check if tbody is empty
         if(table.childElementCount > 0){
+            //Counts existing cells, adds the corresponding amount of cells to 'cells'
             for(i=0; i<table.firstChild.childElementCount; i++){
                 cells += cellString;
             }
+            //Insert row and correct number of cells into the table
             table.insertAdjacentHTML("beforeend", `<tr>${cells}</tr>`);      
         }else{
+            //If table is empty, inject single cell row
             table.insertAdjacentHTML("beforeend", `<tr>${cellString}</tr>`);
         }
     }else{
+        //If table is empty, inject single cell row
         tableTop.insertAdjacentHTML("beforeend", `<tr>${cellString}</tr>`);
     }
     
@@ -26,27 +32,33 @@ function addR() {
 
 // Add a column
 function addC() {
+    //Check if table is empty
     if(tableTop.childElementCount > 0){
         let table = tableTop.firstElementChild;
+        //Check if tbody is empty
         if(table.childElementCount > 0){
+            //Iterate over existing rows, inject one new cell per row
             for(let i = 0; i<table.childElementCount; i++){
                 table.children[i].insertAdjacentHTML("beforeend", cellString);
             }    
         }else{
+            //If table is empty, add single celled row
             addR();
-            addC();
         }
     }else{
+        //If table is empty, add single celled row
         addR();
-        addC();
     }
 }
 
 // Remove a row
 function removeR() {
+    //Check if table is empty
     if(tableTop.childElementCount > 0){
         table = tableTop.firstElementChild;
+        //Check if row exists
         if(table.childElementCount > 0){
+            //Remove last row
             table.removeChild(table.lastChild); 
         }
     }
@@ -54,11 +66,14 @@ function removeR() {
 
 // Remove a column
 function removeC() {
+    //Check if table is empty
     if(tableTop.childElementCount > 0){
         let table = tableTop.firstElementChild;
+        //Iterate over each row, remove the last cell from each
         for(let i = 0; i<table.childElementCount; i++){
             table.children[i].removeChild(table.children[i].lastChild);
         }
+        //Check if rows are empty, if so, remove all rows
         if(table.children[0].childElementCount == 0){
             let rows = table.childElementCount
             for(let i = 0; i<rows; i++){
@@ -74,19 +89,35 @@ function selectColor(){
     console.log(colorSelected);
 }
 
+<<<<<<< Updated upstream
+=======
+// Fill selected cell with selected color
+function fillC(cell){
+    cell.style.backgroundColor = colorSelected
+}
+
+>>>>>>> Stashed changes
 // Fill all uncolored cells
 function fillU(){
+    //Check if table is empty
     if(tableTop.childElementCount > 0){
         let table = tableTop.firstElementChild;
+        //Check if tbody is empty
         if(table.childElementCount > 0){
+            //Iterate over each row
             for(let i = 0; i < table.childElementCount; i++){
+                //Iterate over each cell
                 for(let j = 0; j < table.children[i].childElementCount; j++){
                     cell = table.children[i].children[j];
+                    //Check if cell has any style attributes
                     if(cell.getAttribute("style")){
+                        //If there are stly attributes, check if background color is empty
+                        //set background color to colorSelected if so
                         if(cell.style.backgroundColor == null){
                             cell.style.backgroundColor = colorSelected;    
                         }
                     }else{
+                        //If no style attributes, set background color
                         cell.style.backgroundColor = colorSelected;
                     }
                 }
@@ -98,10 +129,14 @@ function fillU(){
 
 // Fill all cells
 function fillAll(){
+    //Check if table is empty
     if(tableTop.childElementCount > 0){
         let table = tableTop.firstElementChild;
+        //Check if tbody is empty
         if(table.childElementCount > 0){
+            //Iterate each all row
             for(let i = 0; i < table.childElementCount; i++){
+                //Iterate over each cell, set cell background color
                 for(let j = 0; j < table.children[i].childElementCount; j++){
                     cell = table.children[i].children[j];
                     cell.style.backgroundColor = colorSelected;
@@ -114,10 +149,14 @@ function fillAll(){
 
 // Clear all cells
 function clearAll(){
+    //Check if table is empty
     if(tableTop.childElementCount > 0){
         let table = tableTop.firstElementChild;
+        //Check if tbody is empty
         if(table.childElementCount > 0){
+            //Iterate over each row
             for(let i = 0; i < table.childElementCount; i++){
+                //Iterate over each cell, clear cell background color
                 for(let j = 0; j < table.children[i].childElementCount; j++){
                     cell = table.children[i].children[j];
                     cell.style.backgroundColor = null;
